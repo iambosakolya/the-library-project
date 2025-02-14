21aa0ba67cd6d5d966bf698a816fbb7cdea726ec
iambosakolya <olya.bosak06@gmail.com>
2025-02-14 16:15:24 +0200

    data is used(eslint)

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -2,9 +2,17 @@
 
 import { CartItem } from '@/types';
 
+// export async function addItemToCart(data: CartItem) {
+//   return {
+//     success: true,
+//     message: 'item added to cart',
+//   };
+// }
+
 export async function addItemToCart(data: CartItem) {
   return {
     success: true,
-    message: 'item added to cart',
+
+    message: `${data}`,
   };
 }
