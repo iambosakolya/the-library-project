5cfcdaf8d596551fb742639cc9d784a8378b4bba
iambosakolya <olya.bosak06@gmail.com>
2025-02-14 23:43:36 +0200

    getting item from the cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,18 +1,67 @@
 'use server';
 
+import { cookies } from 'next/headers';
 import { CartItem } from '@/types';
-
-// export async function addItemToCart(data: CartItem) {
-//   return {
-//     success: true,
-//     message: 'item added to cart',
-//   };
-// }
+import { convertToPlainObj, formatError } from '../utils';
+import { auth } from '@/auth';
+import { prisma } from '@/db/prisma';
+import { cartItemSchema } from '../validators';
 
 export async function addItemToCart(data: CartItem) {
-  return {
-    success: true,
+  try {
+    // check cart cookie
+    const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+    if (!sessionCartId) throw new Error('No cart found');
 
-    message: `${data}`,
-  };
+    // get session and user id
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    // get cart
+    const cart = await getMyCart();
+    //prse ans validate item
+    const item = cartItemSchema.parse(data);
+
+    // fins product is db
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
+    console.log({
+      'Session Cart id': sessionCartId,
+      'User id': userId,
+      'item requested': item,
+      'certain book': product,
+    });
+
+    return { success: true, message: 'item added to cart' };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function getMyCart() {
+  // check cart cookie
+  const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+  if (!sessionCartId) throw new Error('No cart found');
+
+  // get session and user id
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  return convertToPlainObj({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
