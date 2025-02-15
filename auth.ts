334473da1f02a78c381f15d6100d3ebfdf2cd119
iambosakolya <olya.bosak06@gmail.com>
2025-02-15 03:14:40 +0200

    try

--- auth.ts
+++ auth.ts
@@ -1,11 +1,9 @@
 import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { authConfig } from './auth.config';
 import { prisma } from '@/db/prisma';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { compareSync } from 'bcrypt-ts-edge';
-import type { NextAuthConfig } from 'next-auth';
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { compare } from './lib/encrypt';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const config = {
   pages: {
@@ -13,10 +11,9 @@ export const config = {
     error: '/sign-in',
   },
   session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 
+    strategy: 'jwt' as const,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       credentials: {
@@ -26,20 +23,21 @@ export const config = {
       async authorize(credentials) {
         if (credentials == null) return null;
 
-        // find user in the db
+        // Find user in database
         const user = await prisma.user.findFirst({
           where: {
             email: credentials.email as string,
           },
         });
 
-        // if user exists
+        // Check if user exists and if the password matches
         if (user && user.password) {
-          const isMatch = compareSync(
+          const isMatch = await compare(
             credentials.password as string,
             user.password,
           );
 
+          // If password is correct, return user
           if (isMatch) {
             return {
               id: user.id,
@@ -49,66 +47,76 @@ export const config = {
             };
           }
         }
+        // If user does not exist or password does not match return null
         return null;
       },
     }),
   ],
   callbacks: {
+    ...authConfig.callbacks,
     async session({ session, user, trigger, token }: any) {
-      // set user id from the token
+      // Set the user ID from the token
       session.user.id = token.sub;
       session.user.role = token.role;
       session.user.name = token.name;
 
-      // console.log(token);
-
-      // in case of name update
+      // If there is an update, set the user name
       if (trigger === 'update') {
         session.user.name = user.name;
       }
 
       return session;
     },
-
     async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
+        // If user has no name then use the email
         if (user.name === 'NO_NAME') {
           token.name = user.email!.split('@')[0];
 
+          // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
             data: { name: token.name },
           });
         }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
+
+      // Handle session updates
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
+      }
+
       return token;
     },
-    authorized({ request, auth }: any) {
-      // check for session cart cookie
-      if (!request.cookies.get('sessionCartId')) {
-        // generate new session cart id cookie
-        const sessionCartId = crypto.randomUUID();
-
-        // clone requst headers
-        const newRequestHeaders = new Headers(request.headers);
-
-        // create new response and add the new headers
-        const response = NextResponse.next({
-          request: {
-            headers: newRequestHeaders,
-          },
-        });
-
-        // set newly generated sessionCartId in the response cookies
-        response.cookies.set('sessionCartId', sessionCartId);
-        return response;
-      } else {
-        return true;
-      }
-    },
   },
-} satisfies NextAuthConfig;
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
