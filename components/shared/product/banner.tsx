91b1143f91634fe9329650b2ffb081192d6305a8
iambosakolya <olya.bosak06@gmail.com>
2025-04-09 15:02:15 +0300

    feat: banner updated

--- components/shared/product/banner.tsx
+++ components/shared/product/banner.tsx
@@ -3,15 +3,21 @@ import Image from 'next/image';
 const BannerMain = () => {
   return (
     <div>
+      <div className='flex items-center gap-6 p-4'>
+        <Image
+          src='/images/man-banner.png'
+          alt='main banner'
+          width={600}
+          height={300}
+          priority={true}
+        />
+        <div className=''>
+          <h2 className='text-3xl font-bold'>Welcome to the Library project</h2>
+          <p className='text-xl text-gray-600'>Dive into reading with us</p>
+        </div>
+      </div>
       <Image
-        src='/images/man-banner.png'
-        alt='main banner'
-        width={600}
-        height={300}
-        priority={true}
-      />
-      <Image
-        src='/images/banner.png'  
+        src='/images/banner.png'
         alt='banner'
         width={3000}
         height={300}
