89f9c6102e821e921e1c3dd46de061ff29d62311
Olha Bosak <10252209@stud.op.edu.ua>
2025-05-28 14:40:15 +0300

    fixed method payments

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -26,7 +26,7 @@ export const shippingDetailesDefault = {
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(', ')
-  : ['PayPal', 'Stripe', 'CashOnDelivery'];
+  : ['PayPal', 'CashOnDelivery'];
 export const DEFAULT_PAYMENT_METHOD =
   process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
 
